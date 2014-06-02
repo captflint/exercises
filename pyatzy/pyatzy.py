1285e3107f6ea2ea4dc65169dbb79b7c88ed793f
James Stephenson <c4p7.fl1n7@gmail.com>
2014-06-02 13:52:27 -0400

    implemented dice rolling functions

--- pyatzy/pyatzy.py
+++ pyatzy/pyatzy.py
@@ -4,6 +4,22 @@ def roll(num):
     results = []
     while num > 0:
         results.append(randint(1, 6))
+        num -= 1
     return(results)
 
 def choose(listofrolls):
+    print(listofrolls)
+    timetochoose = True
+    while timetochoose:
+        keeps = input("Which dice do you want to keep: ")
+        klist = []
+        timetochoose = False
+        for n in keeps:
+            if n in '12345':
+                klist.append(listofrolls[int(n) - 1])
+            else:
+                timetochoose = True
+    return(klist + roll(5 - len(klist)))
+
+test = choose(roll(5))
+print(test)
